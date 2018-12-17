f343eb5f8ecde9f3330db4ff58937222e0aec9f3
韦粤信 <283940802@qq.com>
2018-12-18 00:40:04 +0800

    feature: add store/order

--- sportsstore/src/store/index.js
+++ sportsstore/src/store/index.js
@@ -3,6 +3,7 @@ import Vuex from "vuex";
 
 import Axios from "axios";
 import CartModule from './cart'
+import OrdersModule from './orders'
 Vue.use(Vuex);
 
 const baseUrl = "http://localhost:3500";
@@ -11,7 +12,10 @@ const categoriesUrl = `${baseUrl}/categories`;
 
 export default new Vuex.Store({
     strict: true,
-    modules: { cart: CartModule },
+    modules: {
+        cart: CartModule,
+        orders: OrdersModule
+    },
     state: {
         products: [],
         categoriesData: [],
