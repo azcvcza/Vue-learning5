12acecd6e0a131f44075bee00c52b942821c85dd
韦粤信 <283940802@qq.com>
2018-12-18 11:30:18 +0800

    feature: complete order checkout

---
+++ sportsstore/src/components/OrderThanks.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="m-2 text-center">
+		<h2>Thanks!</h2>
+		<p>thanks for placing your order</p>
+		<p>we'll ship your goods soon</p>
+		<router-link to="/" class="btn btn-primary">Return to Store</router-link>
+	</div>
+</template>
+<script>
+export default {
+	computed:{
+		orderId(){
+			return this.$route.params.id;
+		}
+	}
+}
+</script>
--- sportsstore/src/main.js
+++ sportsstore/src/main.js
@@ -3,13 +3,15 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+import store from './store';
+import Vuelidate from "vuelidate";
 Vue.config.productionTip = false
 import "bootstrap/dist/css/bootstrap.min.css";
 import "font-awesome/css/font-awesome.min.css";
 
-import store from './store';
+
 Vue.filter("currency", (value) => new Intl.NumberFormat("en-US", { style: "currency", currency: "USD" }).format(value));
+Vue.use(Vuelidate);
 /* eslint-disable no-new */
 new Vue({
     el: '#app',
