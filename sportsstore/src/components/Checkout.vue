6be3ea4f30f2917d22a85b37abea6f2fdc370d5b
韦粤信 <283940802@qq.com>
2018-12-18 09:20:04 +0800

    feature: add checkout & orderthanks

---
+++ sportsstore/src/components/Checkout.vue
@@ -0,0 +1,35 @@
+<template>
+	<div>
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col bg-dark text-white">
+					<a href class="navbar-brand">Sports store</a>
+				</div>
+			</div>
+		</div>
+		<div class="m-2">
+			<div class="form-group m-2">
+				<label for>Name</label>
+				<input type="text" v-model="name" class="form-control">
+			</div>
+		</div>
+		<div class="text-center">
+			<router-link to="/cart" class="btn btn-secondary m-1">后退</router-link>
+			<button class="btn btn-primary m-1" v-on:click="submitOrder">放置订单</button>
+		</div>
+	</div>
+</template>
+<script>
+export default {
+	data:function(){
+		return {
+			name:null
+		}
+	},
+	methods:{
+		submitOrder(){
+			console.log("submit success");
+		}
+	}
+}
+</script>
--- sportsstore/src/router/index.js
+++ sportsstore/src/router/index.js
@@ -3,13 +3,16 @@ import Router from 'vue-router'
 
 import Store from "../components/Store";
 import ShoppingCart from "../components/ShoppingCart"
-
+import Checkout from "../components/Checkout";
+import OrderThanks from "../components/OrderThanks";
 Vue.use(Router)
 
 export default new Router({
     routes: [
         { path: "/", component: Store },
         { path: "/cart", component: ShoppingCart },
+        { path: "/checkout", component: Checkout },
+        { path: "/thanks/:id", component: OrderThanks },
         { path: "*", redirect: "/" }
     ],
     mode: 'history'
