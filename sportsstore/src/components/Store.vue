4ff3016c51ba9b953f815805ad5cdf3acae3031b
韦粤信 <283940802@qq.com>
2018-12-18 21:31:39 +0800

    feature: add search bar

--- sportsstore/src/components/Store.vue
+++ sportsstore/src/components/Store.vue
@@ -12,24 +12,37 @@
 				<CategoryControls></CategoryControls>
 			</div>
 			<div class="col-9 bg-success p-2">
-				 <router-view/>
-				<h4 class="text-white m-2">PRODUCT</h4>
+				<router-view/>
+
+				<div class="row-2 m-2">
+					
+					<button class="btn btn-block btn-warning" v-on:click="setShowSearch(true)">Search</button>
+					<h4 class="m-2 text-white">Product</h4>
+				</div>
+
 				<div class="col-9 p-2">
-					<ProductList />
+					<Search></Search>
+					<ProductList/>
 				</div>
 			</div>
 		</div>
 	</div>
 </template>
 <script>
-import ProductList from "./ProductList"
-import CategoryControls from "./CategoryControls"
-import CartSummary from "./CartSummary"
-export default {
-	components:{
-		ProductList,
-		CategoryControls,
-		CartSummary
-	}
-}
+	import ProductList from "./ProductList";
+	import CategoryControls from "./CategoryControls";
+	import CartSummary from "./CartSummary";
+	import Search from "./Search";
+	import { mapMutations } from "vuex";
+	export default {
+		components: {
+			ProductList,
+			CategoryControls,
+			CartSummary,
+			Search
+		},
+		methods: {
+			...mapMutations(["setShowSearch"])
+		}
+	};
 </script>
