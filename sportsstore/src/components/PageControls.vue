943d11606db779e4339b7758de8ce93b2215c046
韦粤信 <283940802@qq.com>
2018-12-18 20:46:51 +0800

    feature: modify related methods in store/index.js

--- sportsstore/src/components/PageControls.vue
+++ sportsstore/src/components/PageControls.vue
@@ -34,7 +34,7 @@
     </div>
 </template>
 <script>
-	import { mapState, mapGetters, mapMutations } from "vuex";
+	import { mapState, mapGetters, mapMutations, mapActions } from "vuex";
 	export default {
 		computed: {
 			...mapState(["currentPage"]),
@@ -52,7 +52,8 @@
 			}
 		},
 		methods: {
-			...mapMutations(["setCurrentPage", "setPageSize"]),
+			
+			...mapActions(["setCurrentPage","setPageSize"]),
 			changePageSize($event) {
 				this.setPageSize(Number($event.target.value));
 			}
